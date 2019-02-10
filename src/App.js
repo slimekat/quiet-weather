6c932bb90587097989e6e74f15d6e39710db724d
Sadie Lee <contact@slimekat.com>
2019-02-10 00:48:27 -0500

    replaced geolocation functions

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import Titles from "./components/Titles"
 import Form from "./components/Form"
 import Weather from "./components/Weather"
-import Location from "./components/Location"
 
 const API_KEY = "b7ef65cab5a1a9406d5088b8b1a40e00";
+var celsius;
 
 class App extends React.Component {
-  
+
   state = {
     temperature: undefined,
     city: undefined,
@@ -15,31 +15,41 @@ class App extends React.Component {
     humidity: undefined,
     description: undefined,
     error: undefined,
+    scale: celsius,
+
 
     coords: {
-      latitude:undefined,
-      longitude:undefined,
-      altitude:undefined,
-      accuracy:undefined,
-      altitudeAccuracy:undefined,
-      heading:undefined,
+      latitude: undefined,
+      longitude: undefined,
+      altitude: undefined,
+      accuracy: undefined,
+      altitudeAccuracy: undefined,
+      heading: undefined,
       speed: undefined
     },
 
-    isGeolocationAvailable:undefined,
-    isGeolocationEnabled:undefined,
-    positionError:undefined,
+    isGeolocationAvailable: undefined,
+    isGeolocationEnabled: undefined,
+    positionError: undefined,
   }
 
-  innerRef;
-  setInnerRef(ref) {
-    this.innerRef = ref;
+  tempConvert(inputTemp) {
+    if (this.scale == celsius) {
+      return;
+    } else {
+      inputTemp = (inputTemp + 32);
+      return inputTemp;
+    }
   }
 
-  getLocation() {
-    this.innerRef && this.innerRef.getLocation();
-    // this.Location.getLocation();
-    // Location
+
+  getLocation = async () => {
+    let pos = await new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject)
+    });
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&APPID=${API_KEY}`);
+    const data = await api_call.json();
+    this.updateState(data);
   }
 
   getWeather = async (e) => {
@@ -48,28 +58,32 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
     const data = await api_call.json();
-    if (city && country) {
+    this.updateState(data);
+  }
+
+  updateState(data) {
+    console.log(data);
+    if (data.message) {
       this.setState({
-        temperature:data.main.temp,
-        city:data.name,
-        country:data.sys.country,
-        humidity:data.main.humidity,
-        description:data.weather[0].description,
-        error:""
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: data.message
       });
-      // console.log(data);
     } else {
       this.setState({
-        temperature:undefined,
-        city:undefined,
-        country:undefined,
-        humidity:undefined,
-        description:undefined,
-        error:"Please enter the values"
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
       });
     }
   }
-  render(){
+  render() {
     return (
       <div>
         <div className="wrapper">
@@ -78,24 +92,19 @@ class App extends React.Component {
               <div className="col">
                 <div className="row-xs-2 title-container">
                   <Titles />
-                  {/* <div className="row-xs-3 weather-container"> */}
-                  <Weather 
-                      temperature={this.state.temperature}
-                      city={this.state.city}
-                      country={this.state.country}
-                      humidity={this.state.humidity}
-                      description={this.state.description}
-                      error={this.state.error}
-                    />
-                  {/* </div> */}
-                  
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                    tempConvert={this.tempConvert}
+                  />
                 </div>
                 <div className="row-xs-4 form-container">
-                  <Form getWeather={this.getWeather}/>
-                  {/* <Location getLocation={this.getLocation} /> */}
-                  <Location ref={this.setInnerRef.bind(this)}/>
-                  <button onClick={this.getLocation.bind(this)}>FUCK</button>
-                  {/* <Location getLocation={this.getLocation}/> */}
+                  <Form getWeather={this.getWeather} />
+                  <button onClick={this.getLocation.bind(this)}>X</button>
                 </div>
               </div>
             </div>
@@ -107,6 +116,6 @@ class App extends React.Component {
 };
 
 
-        
+
 
 export default App;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,26 +1,30 @@
 import React from "react";
 
+
+
 const Weather = props => (
     <div className="weather__info">
         <i className="wi wi-owm-721 title-container__weather-icon"></i>
         <div className="weather__output">
             {
-                props.city && props.country && <p className="weather__key">Location: 
+                props.city && props.country && <p className="weather__key">Location:
                     <span className="weather__value"> {props.city},{props.country}</span>
                 </p>
             }
             {
-                props.temperature && <p className="weather__key">Temperature: 
-                    <span className="weather__value"> {props.temperature}</span>
+                props.temperature && <p className="weather__key">Temperature:
+                    {/* <span className="weather__value"> {props.tempConvert(props.temperature - 273.15).toFixed(1)} C</span> */}
+                    <span className="weather__value"> {(props.temperature - 273.15).toFixed(1)} C</span>
+
                 </p>
             }
             {
-                props.humidity && <p className="weather__key">Humidity: 
+                props.humidity && <p className="weather__key">Humidity:
                     <span className="weather__value"> {props.humidity}</span>
                 </p>
             }
             {
-                props.description && <p className="weather__key">Condition: 
+                props.description && <p className="weather__key">Condition:
                     <span className="weather__value"> {props.description}</span>
                 </p>
             }
