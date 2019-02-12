1cee8cd70ec99139eac87728b4e16f73f95d83d7
Sadie Lee <contact@slimekat.com>
2019-02-11 20:23:29 -0500

    moving build to docs

---
+++ docs/precache-manifest.c1d739e4a8f7e3b625315474ca09789b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "26da466aa434617f7821",
+    "url": "./static/css/main.5a3c35e0.chunk.css"
+  },
+  {
+    "revision": "8bef8568339a646b4f4cc5d20ca479f3",
+    "url": "./static/media/weather-app-bg-min.8bef8568.png"
+  },
+  {
+    "revision": "0a5f6a0287ca4462b6ff",
+    "url": "./static/js/1.0a5f6a02.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "26da466aa434617f7821",
+    "url": "./static/js/main.26da466a.chunk.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "./static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "./static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "./static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "./static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "./static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "e14203e4ec1cf3273ed5bd23214f08dc",
+    "url": "./static/media/weather-app-bg.e14203e4.webp"
+  },
+  {
+    "revision": "0a5f6a0287ca4462b6ff",
+    "url": "./static/css/1.003d3151.chunk.css"
+  },
+  {
+    "revision": "15bc8774328ec7e87c69cb834fe8bdba",
+    "url": "./index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.245c4c34392f267e1844abce851f07db.js"
+  "./precache-manifest.c1d739e4a8f7e3b625315474ca09789b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
